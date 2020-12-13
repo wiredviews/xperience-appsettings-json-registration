293f968597c2c45228055c4aae45bdf3ac4be977
Seany Computer <sean@wiredviews.com>
2020-12-13 15:13:47 -0500

    feat(sln): initial commit

---
+++ Xperience.AppSettingsJsonRegistration/AppSettingsJsonRegistrationModule.cs
@@ -0,0 +1,27 @@
+﻿using CMS;
+using CMS.Core;
+using CMS.DataEngine;
+using Microsoft.Extensions.Configuration;
+using XperienceCommunity.AppSettingsJsonRegistration;
+
+[assembly: RegisterModule(typeof(AppSettingsJsonRegistrationModule))]
+
+namespace XperienceCommunity.AppSettingsJsonRegistration
+{
+    public class AppSettingsJsonRegistrationModule : Module
+    {
+        public AppSettingsJsonRegistrationModule() : base(nameof(AppSettingsJsonRegistrationModule))
+        {
+            
+        }
+
+        protected override void OnPreInit()
+        {
+            base.OnPreInit();
+
+            Service.Use<IConfiguration>(() => new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, true)
+                .Build());
+        }
+    }
+}
